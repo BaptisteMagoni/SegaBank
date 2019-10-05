7912597e5e0c48833d5934acc4d63a22d86e47de
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2019-10-05 13:05:15 +0200

    IDAO modif

--- src/net/segabank/dao/IDAO.java
+++ src/net/segabank/dao/IDAO.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface IDAO<TYPE, E> {
+public interface IDAO<TYPE, E, ID> {
 
-    void create(E object, TYPE type) throws SQLException, IOException, ClassNotFoundException;
+    E create(E object, TYPE type) throws SQLException, IOException, ClassNotFoundException;
     void modify(E object, TYPE type) throws SQLException, IOException, ClassNotFoundException;
     void delete(E object) throws SQLException, IOException, ClassNotFoundException;
     List<E> findByTypeCompte(TYPE type) throws SQLException, IOException, ClassNotFoundException;
     List<E> findAll() throws SQLException, IOException, ClassNotFoundException;
+    E findCompteById(ID id);
 
 
 }
