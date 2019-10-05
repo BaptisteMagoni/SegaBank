faf9cbb70357db89634603a05df38623267d4e97
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2019-10-05 19:54:43 +0200

    CSV class

--- src/net/segabank/service/CsvService.java
+++ src/net/segabank/service/CsvService.java
@@ -4,6 +4,7 @@ import org.w3c.dom.css.CSSValue;
 
 import java.io.*;
 import java.nio.Buffer;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 
@@ -17,19 +18,39 @@ public class CsvService {
         try (FileInputStream fis = new FileInputStream(PROPS_FILE)){
             props.load(fis);
         }
-        File file = new File(props.getProperty("scr.directory.file"));
+
+        File file = new File(props.getProperty("scr.directory") + getNameFile() + ".csv");
+
         try{
-            if(!file.exists())
+            if(!file.exists()) {
                 file.createNewFile();
+                writeInCsv(file, "Heure;Action;Somme");
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
 
-        try(FileWriter fw = new FileWriter(file.getPath(), true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw)){
+        writeInCsv(file, getTimeAction() + ";" + args[0] + ";" + args[1]);
 
+    }
+
+    private static void writeInCsv(File file, String data) throws IOException {
+        try(FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw)){
+            bw.write(data + "\n");
         }
+    }
+
+    private static String getNameFile(){
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
+    }
+
+    private static String getTimeAction(){
+        SimpleDateFormat formatter= new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
     }
 
 }
